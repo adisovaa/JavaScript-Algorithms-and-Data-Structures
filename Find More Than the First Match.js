9740e355783270e9bf42afec24648cdfd481b780
lorem <79251934+adisovaa@users.noreply.github.com>
2023-02-28 22:14:42 -0500

    new tasks

---
+++ Find More Than the First Match.js
@@ -0,0 +1,5 @@
+// 27. Find More Than the First Match
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi;
+let result = twinkleStar.match(starRegex);
---
+++ Ignore Case While Matching.js
@@ -0,0 +1,5 @@
+// 25. Ignore Case While Matching
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
