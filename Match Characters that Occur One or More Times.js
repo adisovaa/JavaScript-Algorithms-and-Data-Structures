cd8b63c94a50de519f94eea2ea6c555bcbd52bdc
lorem <79251934+adisovaa@users.noreply.github.com>
2023-02-28 23:13:14 -0500

    new task

---
+++ Match Characters that Occur One or More Times.js
@@ -0,0 +1,5 @@
+// 32. Match Characters that Occur One or More Times
+
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/g; // this is the solution
+let result = difficultSpelling.match(myRegex);
---
+++ Match Single Characters Not Specified.js
@@ -0,0 +1,5 @@
+// 31. Match Single Characters Not Specified
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou^0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
